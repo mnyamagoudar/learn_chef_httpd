eaa1bdeac2b2f20c68735a9cbaebc1060af05141
mnyamagoudar <mnyamagoudar@msystechnologies.com>
2021-09-20 11:06:06 +0530

    initial setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+
+
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
